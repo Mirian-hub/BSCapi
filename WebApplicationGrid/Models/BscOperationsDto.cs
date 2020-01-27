26c9174a3a4d18f11e8cec34cee3bdedb744ac8a
Mirian <Net@fina2.net>
2020-01-27 11:40:05 +0400

    UI_1/27/2020

---
+++ WebApplicationGrid/Models/BscOperationsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace WebApplicationGrid.Models
+{
+    public class BSCOperationsDto
+    {
+        public IEnumerable<BscOperation> Opearations { get; set; }
+        public List<string> PropNames { get; set; }
+        public List<List<object>> PropertyValues { get; set; }
+        public List<object> FinalPropertyValues { get; set; }
+        public string Sourse { get; set; }
+        public List<string> PropertyDataTypes { get; set; }
+    }
+
+
+}
